9352d0e0ff89fedfe550b283d96538b9d353135d
zare <18870652909@163.com>
2018-11-27 18:56:11 +0800

    1.路由监听 进入死循环问题解决~~~~~~~~~~~~~~~~~~

--- src/router/index.js
+++ src/router/index.js
@@ -78,7 +78,7 @@ export const constantRouterMap = [
 ]
 
 export default new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
