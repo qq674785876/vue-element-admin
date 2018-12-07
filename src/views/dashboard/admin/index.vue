6400961264f6297154299bbbd7ba5016ec69c331
zare <18870652909@163.com>
2018-12-07 09:25:27 +0800

    首页增加图标。样式优化~~~~~~~~~~~~~

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="dashboard-editor-container">
     <el-row v-if="device==='mobile'" class="exhibition-box">
-      <el-col v-for="(list, index) in exhibition" :span="24" :style="{'background-image': 'url('+ list.bg +')'}" :key="index" class="exhibition-list">
-        <p class="title">{{ list.title }}</p>
-        <p class="tips">{{ list.tips }}</p>
+      <el-col v-for="(list, index) in exhibition" :span="24" :style="{'background-color': list.bgColor }" :key="index" class="exhibition-list">
+        <div class="exhibition-cont">
+          <span class="svg-container">
+            <svg-icon :icon-class="list.svgClass" />
+          </span>
+          <p class="title">{{ list.title }}</p>
+          <p class="tips">{{ list.tips }}</p>
+        </div>
       </el-col>
     </el-row>
 
     <el-row v-if="device!=='mobile'" class="exhibition-box" type="flex" justify="space-around">
-      <el-col v-for="(list, index) in exhibition" :span="5" :style="{'background-image': 'url('+ list.bg +')'}" :key="index" class="exhibition-list">
-        <p class="title">{{ list.title }}</p>
-        <p class="tips">{{ list.tips }}</p>
+      <el-col v-for="(list, index) in exhibition" :span="5" :style="{'background-color': list.bgColor }" :key="index" class="exhibition-list">
+        <div class="exhibition-cont">
+          <span class="svg-container">
+            <svg-icon :icon-class="list.svgClass" />
+          </span>
+          <p class="title">{{ list.title }}</p>
+          <p class="tips">{{ list.tips }}</p>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -25,17 +35,20 @@ export default {
   data() {
     return {
       exhibition: [{
-        bg: '',
+        bgColor: 'rgb(5, 171, 191)',
         title: '内测托管',
-        tips: '一键上传应用，扫描二维码下载'
+        tips: '一键上传应用，扫描二维码下载',
+        svgClass: 'tuoguan'
       }, {
-        bg: '',
+        bgColor: 'rgb(94, 90, 226)',
         title: '应用合并',
-        tips: '扫描同一个二维码，根据设备类型自动下载对应的IOS或Android应用'
+        tips: '扫描同一个二维码，根据设备类型自动下载对应的IOS或Android应用',
+        svgClass: 'merge'
       }, {
-        bg: '',
+        bgColor: 'rgb(54, 59, 61)',
         title: '实名管理',
-        tips: '实名认证保证每个上传者在一个安全的环境中进行应用开发'
+        tips: '实名认证保证每个上传者在一个安全的环境中进行应用开发',
+        svgClass: 'realName'
       }]
     }
   },
@@ -61,11 +74,17 @@ export default {
     .exhibition-list{
       height: 60vh;
       background-size: cover;
-      background-color: #376AFB;
       padding: 0 40px;
+      border-radius: 10px;
+      .exhibition-cont{
+        margin-top: 15vh;
+        color: #fff;
+        .svg-container{
+          font-size: 10vh;
+        }
+      }
       .title{
         font-size: 26px;
-        margin-top: 70%;
       }
     }
   }
