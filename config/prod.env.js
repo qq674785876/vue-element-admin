e98f9ecb253257f2281a06191c3490c8667bb6a2
zare <18870652909@163.com>
2018-12-14 20:58:27 +0800

    下载页面优化

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,4 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"https://api.ublog.top"'
+  // BASE_API: '"https://www.tapk.top/s"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -2,4 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
   BASE_API: '"https://api.ublog.top"'
+  // BASE_API: '"https://www.tapk.top/s"'
 }
