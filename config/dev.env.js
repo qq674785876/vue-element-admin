cd27fa99afd2b060e88cd36d1f573c3e5474d7e9
张震 <1191@mapgoo.net>
2019-03-06 09:03:54 +0800

    1.vue配置文件更新~~~~~~~~~~~~~~

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   // BASE_API: '"https://www.tapk.top/s"'
   // BASE_API: '"https://api.ublog.top"'
-  BASE_API: '"https://www.tapk.top/s"'
+  BASE_API: '"https://app.ipahc.com/s"'
 }
--- config/sit.env.js
+++ config/sit.env.js
@@ -1,7 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  // BASE_API: '"https://www.tapk.top/s"'
   // BASE_API: '"https://api.ublog.top"'
-  BASE_API: '"https://www.tapk.top/s"'
+  // BASE_API: '"https://www.tapk.top/s"'
+  BASE_API: '"https://app.ipahc.com/s"'
 }
