ba2a52a16eda4806ce3ab9a4c39edb9b1366bcbe
张震 <1191@mapgoo.net>
2019-06-27 09:26:14 +0800

    1.代码提交~~~~~~~~~~~~~~~~

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   // BASE_API: '"https://www.tapk.top/s"'
-  BASE_API: '"https://app.ipahc.com/s"'
+  BASE_API: '"http://106.13.66.152:8686"'
 }
